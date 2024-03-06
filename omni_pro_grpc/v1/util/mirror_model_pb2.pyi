2d9731285fa46111de4d13e8c1a2204f83c1bacd
GitHub Action Deploy <deploy.action@omni.pro>
2024-03-06 20:22:34 +0000

    Generated protos for library deployment, Merge pull request #618 from Omnipro-Solutions/develop

---
+++ omni_pro_grpc/v1/util/mirror_model_pb2.pyi
@@ -0,0 +1,60 @@
+from typing import ClassVar as _ClassVar
+from typing import Mapping as _Mapping
+from typing import Optional as _Optional
+from typing import Union as _Union
+
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from google.protobuf import struct_pb2 as _struct_pb2
+from omni_pro_grpc.common import base_pb2 as _base_pb2
+from omni_pro_grpc.v1.utilities import model_pb2 as _model_pb2
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class CreateOrUpdateMirrorModel(_message.Message):
+    __slots__ = ["model_path", "model_data", "context"]
+    MODEL_PATH_FIELD_NUMBER: _ClassVar[int]
+    MODEL_DATA_FIELD_NUMBER: _ClassVar[int]
+    CONTEXT_FIELD_NUMBER: _ClassVar[int]
+    model_path: str
+    model_data: _struct_pb2.Struct
+    context: _base_pb2.Context
+    def __init__(
+        self,
+        model_path: _Optional[str] = ...,
+        model_data: _Optional[_Union[_struct_pb2.Struct, _Mapping]] = ...,
+        context: _Optional[_Union[_base_pb2.Context, _Mapping]] = ...,
+    ) -> None: ...
+
+class CreateOrUpdateCreateMirrorResponse(_message.Message):
+    __slots__ = ["model_data", "response_standard"]
+    MODEL_DATA_FIELD_NUMBER: _ClassVar[int]
+    RESPONSE_STANDARD_FIELD_NUMBER: _ClassVar[int]
+    model_data: _struct_pb2.Struct
+    response_standard: _base_pb2.ResponseStandard
+    def __init__(
+        self,
+        model_data: _Optional[_Union[_struct_pb2.Struct, _Mapping]] = ...,
+        response_standard: _Optional[_Union[_base_pb2.ResponseStandard, _Mapping]] = ...,
+    ) -> None: ...
+
+class DeleteMirrorModelRequest(_message.Message):
+    __slots__ = ["model_path", "id", "context"]
+    MODEL_PATH_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    CONTEXT_FIELD_NUMBER: _ClassVar[int]
+    model_path: str
+    id: str
+    context: _base_pb2.Context
+    def __init__(
+        self,
+        model_path: _Optional[str] = ...,
+        id: _Optional[str] = ...,
+        context: _Optional[_Union[_base_pb2.Context, _Mapping]] = ...,
+    ) -> None: ...
+
+class DeleteMirrorModelResponse(_message.Message):
+    __slots__ = ["response_standard"]
+    RESPONSE_STANDARD_FIELD_NUMBER: _ClassVar[int]
+    response_standard: _base_pb2.ResponseStandard
+    def __init__(self, response_standard: _Optional[_Union[_base_pb2.ResponseStandard, _Mapping]] = ...) -> None: ...
