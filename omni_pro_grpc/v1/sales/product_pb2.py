f584977ccea6fd52f950103d0a117f7f0c4bf7d6
GitHub Action Deploy <deploy.action@omni.pro>
2024-05-22 21:53:45 +0000

    Generated protos for library deployment, Merge pull request #747 from Omnipro-Solutions/integration

--- omni_pro_grpc/v1/sales/product_pb2.py
+++ omni_pro_grpc/v1/sales/product_pb2.py
@@ -12,11 +12,12 @@ from google.protobuf.internal import builder as _builder
 _sym_db = _symbol_database.Default()
 
 
+from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 from google.protobuf import wrappers_pb2 as google_dot_protobuf_dot_wrappers__pb2
 from omni_pro_grpc.common import base_pb2 as common_dot_base__pb2
 
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x16v1/sales/product.proto\x12!pro.omni.oms.api.v1.sales.product\x1a\x11\x63ommon/base.proto\x1a\x1egoogle/protobuf/wrappers.proto"\xe3\x01\n\x07Product\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x16\n\x0eproduct_doc_id\x18\x02 \x01(\t\x12\x17\n\x0ftemplate_doc_id\x18\x03 \x01(\t\x12\x0c\n\x04name\x18\x04 \x01(\t\x12\x0b\n\x03sku\x18\x05 \x01(\t\x12*\n\x06\x61\x63tive\x18\x06 \x01(\x0b\x32\x1a.google.protobuf.BoolValue\x12\x13\n\x0b\x65xternal_id\x18\x07 \x01(\t\x12?\n\x0cobject_audit\x18\x08 \x01(\x0b\x32).pro.omni.oms.api.common.base.ObjectAudit"\xaf\x01\n\x14ProductCreateRequest\x12\x16\n\x0eproduct_doc_id\x18\x01 \x01(\t\x12\x17\n\x0ftemplate_doc_id\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\x0b\n\x03sku\x18\x04 \x01(\t\x12\x13\n\x0b\x65xternal_id\x18\x05 \x01(\t\x12\x36\n\x07\x63ontext\x18\x06 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"\x9f\x01\n\x15ProductCreateResponse\x12;\n\x07product\x18\x01 \x01(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12I\n\x11response_standard\x18\x02 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard"\xf0\x02\n\x12ProductReadRequest\x12\x37\n\x08group_by\x18\x01 \x03(\x0b\x32%.pro.omni.oms.api.common.base.GroupBy\x12\x35\n\x07sort_by\x18\x02 \x01(\x0b\x32$.pro.omni.oms.api.common.base.SortBy\x12\x34\n\x06\x66ields\x18\x03 \x01(\x0b\x32$.pro.omni.oms.api.common.base.Fields\x12\x34\n\x06\x66ilter\x18\x04 \x01(\x0b\x32$.pro.omni.oms.api.common.base.Filter\x12:\n\tpaginated\x18\x05 \x01(\x0b\x32\'.pro.omni.oms.api.common.base.Paginated\x12\n\n\x02id\x18\x06 \x01(\x05\x12\x36\n\x07\x63ontext\x18\x07 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"\xd9\x01\n\x13ProductReadResponse\x12<\n\x08products\x18\x01 \x03(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12I\n\x11response_standard\x18\x02 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard\x12\x39\n\tmeta_data\x18\x03 \x01(\x0b\x32&.pro.omni.oms.api.common.base.MetaData"\x8b\x01\n\x14ProductUpdateRequest\x12;\n\x07product\x18\x01 \x01(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12\x36\n\x07\x63ontext\x18\x02 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"\x9f\x01\n\x15ProductUpdateResponse\x12;\n\x07product\x18\x01 \x01(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12I\n\x11response_standard\x18\x02 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard"Z\n\x14ProductDeleteRequest\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x36\n\x07\x63ontext\x18\x02 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"b\n\x15ProductDeleteResponse\x12I\n\x11response_standard\x18\x01 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard2\xa5\x04\n\x0eProductService\x12\x84\x01\n\rProductCreate\x12\x37.pro.omni.oms.api.v1.sales.product.ProductCreateRequest\x1a\x38.pro.omni.oms.api.v1.sales.product.ProductCreateResponse"\x00\x12~\n\x0bProductRead\x12\x35.pro.omni.oms.api.v1.sales.product.ProductReadRequest\x1a\x36.pro.omni.oms.api.v1.sales.product.ProductReadResponse"\x00\x12\x84\x01\n\rProductUpdate\x12\x37.pro.omni.oms.api.v1.sales.product.ProductUpdateRequest\x1a\x38.pro.omni.oms.api.v1.sales.product.ProductUpdateResponse"\x00\x12\x84\x01\n\rProductDelete\x12\x37.pro.omni.oms.api.v1.sales.product.ProductDeleteRequest\x1a\x38.pro.omni.oms.api.v1.sales.product.ProductDeleteResponse"\x00\x62\x06proto3'
+    b'\n\x16v1/sales/product.proto\x12!pro.omni.oms.api.v1.sales.product\x1a\x11\x63ommon/base.proto\x1a\x1egoogle/protobuf/wrappers.proto\x1a\x1cgoogle/protobuf/struct.proto"\x8b\x02\n\x07Product\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x16\n\x0eproduct_doc_id\x18\x02 \x01(\t\x12\x17\n\x0ftemplate_doc_id\x18\x03 \x01(\t\x12\x0c\n\x04name\x18\x04 \x01(\t\x12\x0b\n\x03sku\x18\x05 \x01(\t\x12&\n\x05image\x18\x06 \x01(\x0b\x32\x17.google.protobuf.Struct\x12*\n\x06\x61\x63tive\x18\x07 \x01(\x0b\x32\x1a.google.protobuf.BoolValue\x12\x13\n\x0b\x65xternal_id\x18\x08 \x01(\t\x12?\n\x0cobject_audit\x18\t \x01(\x0b\x32).pro.omni.oms.api.common.base.ObjectAudit"\xaf\x01\n\x14ProductCreateRequest\x12\x16\n\x0eproduct_doc_id\x18\x01 \x01(\t\x12\x17\n\x0ftemplate_doc_id\x18\x02 \x01(\t\x12\x0c\n\x04name\x18\x03 \x01(\t\x12\x0b\n\x03sku\x18\x04 \x01(\t\x12\x13\n\x0b\x65xternal_id\x18\x05 \x01(\t\x12\x36\n\x07\x63ontext\x18\x06 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"\x9f\x01\n\x15ProductCreateResponse\x12;\n\x07product\x18\x01 \x01(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12I\n\x11response_standard\x18\x02 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard"\xf0\x02\n\x12ProductReadRequest\x12\x37\n\x08group_by\x18\x01 \x03(\x0b\x32%.pro.omni.oms.api.common.base.GroupBy\x12\x35\n\x07sort_by\x18\x02 \x01(\x0b\x32$.pro.omni.oms.api.common.base.SortBy\x12\x34\n\x06\x66ields\x18\x03 \x01(\x0b\x32$.pro.omni.oms.api.common.base.Fields\x12\x34\n\x06\x66ilter\x18\x04 \x01(\x0b\x32$.pro.omni.oms.api.common.base.Filter\x12:\n\tpaginated\x18\x05 \x01(\x0b\x32\'.pro.omni.oms.api.common.base.Paginated\x12\n\n\x02id\x18\x06 \x01(\x05\x12\x36\n\x07\x63ontext\x18\x07 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"\xd9\x01\n\x13ProductReadResponse\x12<\n\x08products\x18\x01 \x03(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12I\n\x11response_standard\x18\x02 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard\x12\x39\n\tmeta_data\x18\x03 \x01(\x0b\x32&.pro.omni.oms.api.common.base.MetaData"\x8b\x01\n\x14ProductUpdateRequest\x12;\n\x07product\x18\x01 \x01(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12\x36\n\x07\x63ontext\x18\x02 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"\x9f\x01\n\x15ProductUpdateResponse\x12;\n\x07product\x18\x01 \x01(\x0b\x32*.pro.omni.oms.api.v1.sales.product.Product\x12I\n\x11response_standard\x18\x02 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard"Z\n\x14ProductDeleteRequest\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x36\n\x07\x63ontext\x18\x02 \x01(\x0b\x32%.pro.omni.oms.api.common.base.Context"b\n\x15ProductDeleteResponse\x12I\n\x11response_standard\x18\x01 \x01(\x0b\x32..pro.omni.oms.api.common.base.ResponseStandard2\xa5\x04\n\x0eProductService\x12\x84\x01\n\rProductCreate\x12\x37.pro.omni.oms.api.v1.sales.product.ProductCreateRequest\x1a\x38.pro.omni.oms.api.v1.sales.product.ProductCreateResponse"\x00\x12~\n\x0bProductRead\x12\x35.pro.omni.oms.api.v1.sales.product.ProductReadRequest\x1a\x36.pro.omni.oms.api.v1.sales.product.ProductReadResponse"\x00\x12\x84\x01\n\rProductUpdate\x12\x37.pro.omni.oms.api.v1.sales.product.ProductUpdateRequest\x1a\x38.pro.omni.oms.api.v1.sales.product.ProductUpdateResponse"\x00\x12\x84\x01\n\rProductDelete\x12\x37.pro.omni.oms.api.v1.sales.product.ProductDeleteRequest\x1a\x38.pro.omni.oms.api.v1.sales.product.ProductDeleteResponse"\x00\x62\x06proto3'
 )
 
 _globals = globals()
@@ -25,24 +26,24 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "v1.sales.product_pb2", _glo
 if _descriptor._USE_C_DESCRIPTORS == False:
 
     DESCRIPTOR._options = None
-    _globals["_PRODUCT"]._serialized_start = 113
-    _globals["_PRODUCT"]._serialized_end = 340
-    _globals["_PRODUCTCREATEREQUEST"]._serialized_start = 343
-    _globals["_PRODUCTCREATEREQUEST"]._serialized_end = 518
-    _globals["_PRODUCTCREATERESPONSE"]._serialized_start = 521
-    _globals["_PRODUCTCREATERESPONSE"]._serialized_end = 680
-    _globals["_PRODUCTREADREQUEST"]._serialized_start = 683
-    _globals["_PRODUCTREADREQUEST"]._serialized_end = 1051
-    _globals["_PRODUCTREADRESPONSE"]._serialized_start = 1054
-    _globals["_PRODUCTREADRESPONSE"]._serialized_end = 1271
-    _globals["_PRODUCTUPDATEREQUEST"]._serialized_start = 1274
-    _globals["_PRODUCTUPDATEREQUEST"]._serialized_end = 1413
-    _globals["_PRODUCTUPDATERESPONSE"]._serialized_start = 1416
-    _globals["_PRODUCTUPDATERESPONSE"]._serialized_end = 1575
-    _globals["_PRODUCTDELETEREQUEST"]._serialized_start = 1577
-    _globals["_PRODUCTDELETEREQUEST"]._serialized_end = 1667
-    _globals["_PRODUCTDELETERESPONSE"]._serialized_start = 1669
-    _globals["_PRODUCTDELETERESPONSE"]._serialized_end = 1767
-    _globals["_PRODUCTSERVICE"]._serialized_start = 1770
-    _globals["_PRODUCTSERVICE"]._serialized_end = 2319
+    _globals["_PRODUCT"]._serialized_start = 143
+    _globals["_PRODUCT"]._serialized_end = 410
+    _globals["_PRODUCTCREATEREQUEST"]._serialized_start = 413
+    _globals["_PRODUCTCREATEREQUEST"]._serialized_end = 588
+    _globals["_PRODUCTCREATERESPONSE"]._serialized_start = 591
+    _globals["_PRODUCTCREATERESPONSE"]._serialized_end = 750
+    _globals["_PRODUCTREADREQUEST"]._serialized_start = 753
+    _globals["_PRODUCTREADREQUEST"]._serialized_end = 1121
+    _globals["_PRODUCTREADRESPONSE"]._serialized_start = 1124
+    _globals["_PRODUCTREADRESPONSE"]._serialized_end = 1341
+    _globals["_PRODUCTUPDATEREQUEST"]._serialized_start = 1344
+    _globals["_PRODUCTUPDATEREQUEST"]._serialized_end = 1483
+    _globals["_PRODUCTUPDATERESPONSE"]._serialized_start = 1486
+    _globals["_PRODUCTUPDATERESPONSE"]._serialized_end = 1645
+    _globals["_PRODUCTDELETEREQUEST"]._serialized_start = 1647
+    _globals["_PRODUCTDELETEREQUEST"]._serialized_end = 1737
+    _globals["_PRODUCTDELETERESPONSE"]._serialized_start = 1739
+    _globals["_PRODUCTDELETERESPONSE"]._serialized_end = 1837
+    _globals["_PRODUCTSERVICE"]._serialized_start = 1840
+    _globals["_PRODUCTSERVICE"]._serialized_end = 2389
 # @@protoc_insertion_point(module_scope)
