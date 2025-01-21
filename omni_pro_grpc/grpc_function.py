63c5853fe2451cc067611d01669791fbb6ed6586
Edgar Elías Peña De La Torre <edgar.pena@omni.pro>
2025-01-21 09:34:58 -0500

    Add OrderRPCFunction class with read_order method for order retrieval

--- omni_pro_grpc/grpc_function.py
+++ omni_pro_grpc/grpc_function.py
@@ -757,3 +757,40 @@ class PickingRPCFunction(object):
             )
         )
         return self.client.call_rpc_fuction(self.event, cache=self.cache) + (self.event,)
+
+
+class OrderRPCFunction(object):
+
+    def __init__(self, context: dict, cache: bool = False) -> None:
+        """
+        :param context: context with tenant and user\n
+        Example:
+        ```
+        context = {"tenant": "tenant_code", "user": "user_name"}
+        """
+        self.context = context
+        self.cache = cache
+        self.service_id = MicroService.SAAS_MS_SALE.value
+        self.module_grpc = "v1.sales.order_pb2_grpc"
+        self.stub_classname = "OrderServiceStub"
+        self.module_pb2 = "v1.sales.order_pb2"
+
+        self.event: Event = Event(
+            module_grpc=self.module_grpc,
+            stub_classname=self.stub_classname,
+            module_pb2=self.module_pb2,
+            rpc_method=None,
+            request_class=None,
+        )
+
+        self.client: GRPClient = GRPClient(self.service_id)
+
+    def read_order(self, params: dict):
+        self.event.update(
+            dict(
+                rpc_method="OrderRead",
+                request_class="OrderReadRequest",
+                params={"context": self.context} | params,
+            )
+        )
+        return self.client.call_rpc_fuction(self.event, cache=self.cache) + (self.event,)
